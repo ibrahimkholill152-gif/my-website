b3321d266b1b1eb268dba31e1e92e422dc38d944
ibrahimkholill152-gif <ibrahimkholill152@gmail.com>
2026-04-27 20:30:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -333,6 +333,48 @@ function enableThumbScroll(){
     });
 }
 
+function enableMainSliderDrag(){
+
+    const slider = document.getElementById("momenSlider");
+    if(!slider) return;
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener("mousedown",(e)=>{
+        isDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener("mouseleave",()=> isDown=false);
+    slider.addEventListener("mouseup",()=> isDown=false);
+
+    slider.addEventListener("mousemove",(e)=>{
+        if(!isDown) return;
+
+        e.preventDefault();
+
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2;
+
+        slider.scrollLeft = scrollLeft - walk;
+    });
+
+    // TOUCH HP
+    slider.addEventListener("touchstart",(e)=>{
+        startX = e.touches[0].pageX;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener("touchmove",(e)=>{
+        const x = e.touches[0].pageX;
+        const walk = (x - startX) * 2;
+
+        slider.scrollLeft = scrollLeft - walk;
+    });
+}
 // ============================================
 // INIT
 // ============================================
